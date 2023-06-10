847de629ec5c0caea7787f2a6d88ccd7cd137127
alnahdi7 <m.alnahdi252@gmail.com>
2023-06-10 19:01:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+Console.WriteLine("Enter Frist number ");
+    double number1 =double.Parse(Console.ReadLine());
+Console.WriteLine("Choees The Opration ");
+ char Opration = char.Parse(Console.ReadLine());
+Console.WriteLine("Enter Frist number ");
+double number2 = double.Parse(Console.ReadLine());
+if (Opration == '+'|| Opration == '-' || Opration == '*' || Opration == '/' )
+{
+
+    double total;
+
+    switch (Opration)
+    {
+       case '+':  total = number1 + number2;
+            Console.WriteLine("The totle is :"+total+"");
+            break;
+        case '-':  total = number1 + number2;
+            Console.WriteLine("The totle is :" + total + "");
+            break;
+        case '*':
+            total = number1 * number2;
+            Console.WriteLine("The totle is :" + total + "");
+            break;
+        case '/':
+            if (number2 == 0)
+            {
+
+                Console.WriteLine("Can not diveion by zero");
+
+            }
+            else
+            {
+                total = number1 / number2;
+                Console.WriteLine("The totle is :" + total + "");
+            }
+            break;
+default:
+        Console.Write("Invalid Opration tray agin ");
+        break;  
+
+    }
+
+
+
+}
+else
+    Console.WriteLine("The opration is erro ");
